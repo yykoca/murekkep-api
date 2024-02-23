f5d1d1484ab576df556c20db5a130ce554211704
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-02-23 15:51:45 +0100

    Add some new style and dynamic data in to homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+
+        $articles = $this->entityManager->getRepository(Article::class)->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'articles'  => $articles,
         ]);
     }
 }
